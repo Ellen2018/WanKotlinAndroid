44c6b6e69771a0671fb7cf630240e479fc60860c
Ellen2018 <1961075191@qq.com>
2022-05-19 09:00:15 +0800

    主页数据请求放在ViewModel里

--- app/src/main/java/com/yalemang/wankotlinandroid/ui/main/MainActivity.kt
+++ app/src/main/java/com/yalemang/wankotlinandroid/ui/main/MainActivity.kt
@@ -1,30 +1,27 @@
 package com.yalemang.wankotlinandroid.ui.main
 
-import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.google.gson.Gson
 import com.yalemang.wankotlinandroid.R
 import com.yalemang.wankotlinandroid.base.BaseActivity
-import com.yalemang.wankotlinandroid.http.RetrofitManager
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 /**
  * 主界面
  */
 class MainActivity:BaseActivity() {
+
+    private lateinit var mainViewModel:MainViewModel
+
     override fun initView() {
 
     }
 
     override fun initData() {
-        GlobalScope.launch(Dispatchers.Main) {
-            //获取首页数据
-            val data = RetrofitManager.getInstance().getApi().getBannerData()
-            Log.d("Ellen2018", "json数据:${Gson().toJson(data)}")
-            tv_content.text = Gson().toJson(data)
-        }
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        mainViewModel.mainData.observe(this,{
+            tv_content.text = Gson().toJson(it)
+        })
 
     }
 
--- app/src/main/java/com/yalemang/wankotlinandroid/ui/main/MainViewModel.kt
+++ app/src/main/java/com/yalemang/wankotlinandroid/ui/main/MainViewModel.kt
@@ -1,6 +1,28 @@
 package com.yalemang.wankotlinandroid.ui.main
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.yalemang.wankotlinandroid.bean.MainData
+import com.yalemang.wankotlinandroid.http.RetrofitManager
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainViewModel:ViewModel() {
+
+    val mainData:MutableLiveData<MainData> by lazy {
+        MutableLiveData<MainData>().also {
+             loadMainData()
+        }
+    }
+
+    private fun loadMainData(){
+
+        GlobalScope.launch() {
+            //获取首页数据
+            Log.d("Ellen2020","当前线程:${Thread.currentThread().name}")
+            val data = RetrofitManager.getInstance().getApi().getBannerData()
+            mainData.postValue(data)
+        }
+    }
 }
