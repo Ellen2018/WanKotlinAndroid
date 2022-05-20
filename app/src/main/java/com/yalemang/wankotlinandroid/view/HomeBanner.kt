b0166af83c331e61590d93efc4bb109789ec4fae
Ellen2018 <1961075191@qq.com>
2022-05-20 17:13:03 +0800

    完成首页Banner图

--- app/src/main/java/com/yalemang/wankotlinandroid/view/HomeBanner.kt
+++ app/src/main/java/com/yalemang/wankotlinandroid/view/HomeBanner.kt
@@ -3,7 +3,6 @@ package com.yalemang.wankotlinandroid.view
 import android.content.Context
 import android.util.AttributeSet
 import com.yalemang.wankotlinandroid.bean.DataBanner
-import com.yalemang.wankotlinandroid.ui.main.fragment.home.adapter.BannerViewHolder
 import com.yalemang.wankotlinandroid.ui.main.fragment.home.adapter.HomeBannerAdapter
 import com.youth.banner.Banner
 
@@ -11,6 +10,6 @@ import com.youth.banner.Banner
  * 兼容Java泛型封装一层解决Java泛型与Kotlin不兼容问题
  */
 class HomeBanner(context: Context?, attrs: AttributeSet?) :
-    Banner<DataBanner, HomeBannerAdapter<DataBanner, BannerViewHolder>>(context, attrs) {
+    Banner<DataBanner, HomeBannerAdapter>(context, attrs) {
 
 }
